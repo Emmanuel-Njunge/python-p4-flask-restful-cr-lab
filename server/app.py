dda8b1575ed0bd97e3aa52d7b6edd35c35098fcf
Emmanuel Njungé Ngugi <ngugiemmanuelnjunge@gmail.com>
2025-06-21 20:11:32 +0300

    lab

--- server/app.py
+++ server/app.py
@@ -17,10 +17,56 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plant_list=[plant.to_dict() for plant in Plant.query.all()]
+        response=make_response(
+            jsonify(plant_list),
+            200
+        )
+
+        return response
+    
+
+    def post(self):
+        data =request.get_json()
+        added_plant=Plant(
+            name=data.get('name'),
+            image=data.get('image'),
+            price=data.get('price')
+        )
+        db.session.add(added_plant)
+        db.session.commit()
+
+        response_body=added_plant.to_dict()
+
+        response=make_response(
+            response_body,
+            201
+        )
+
+        return response
+
+api.add_resource(Plants,'/plants')
+
+
 
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+        plant=Plant.query.get(id)
+        response_body=plant.to_dict()
+
+        response=make_response(
+            jsonify(response_body),
+            200
+        )
+
+        return response
+
+
+api.add_resource(PlantByID,'/plants/<int:id>')
+
+
+
         
 
 if __name__ == '__main__':
